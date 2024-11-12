7e807ae0ef656ef90816d53efb92155153169a01
Bruna Oliveira <bruna.oliveira@ibbi.org.br>
2024-11-12 13:05:31 -0400

    adicionando teclas,audio e editando

---
+++ src/scripts/engine.js
@@ -0,0 +1,39 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+
+
+let mapedKeys = [];
+let audio = new Audio("src/tunes/A.wav");
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`;
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(()=>{
+        clickedKey.classList.remove("active");
+
+    },150);
+
+};
+
+
+pianoKeys.forEach((key)=>{
+    key.addEventListener("click",()=> playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
+    
+});
+
+document.addEventListener("keydown", (E) => {
+    const key = E.key.toUpperCase(); 
+    if (mapedKeys.includes(key)) {  
+        playTune(key);
+    }
+});
+
+const handleVolume = (E) =>{
+    audio.volume = E.target.value
+   
+}
+
+volumeSlider.addEventListener("input", handleVolume)
